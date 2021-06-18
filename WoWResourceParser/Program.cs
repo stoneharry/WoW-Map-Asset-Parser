df0b3a323ec874a078eb9d1d83db938a7d06eaeb
stoneharry <stoneharryjames@gmail.com>
2021-06-18 23:48:44 +0100

    Multiple bug fixes - Copy Skin and BLP files for all M2 files found - Read the MOMT chunk for offsets into the MOTX chunk - Read the ADT MTEX chunk

--- WoWResourceParser/Program.cs
+++ WoWResourceParser/Program.cs
@@ -17,7 +17,7 @@ namespace WoWResourceParser
             string dataFolder = "E:\\_NewProjectWoW\\_HOCKA\\mpqs";
             string assetsFilePath = "assets.txt";
 
-            string destFolder = "E:\\_NewProjectWoW\\_Package";
+            string destFolder = "D:\\WoW 3.3.5a\\Data\\patch-5";
 
             if (extract)
             {
@@ -83,7 +83,7 @@ ADT Folder:" + $"\n {adtFolder}\n" +
             var blps = new List<string>();
 
             Console.WriteLine("Parsing all ADTs...");
-            ParseAllADTs(ref wmos, ref m2s, adtFolder);
+            ParseAllADTs(ref wmos, ref m2s, ref blps, adtFolder);
 
             Console.WriteLine("Parsing all WMOs...");
             ParseAllWMOs(wmos, ref m2s, ref blps, dataFolder);
@@ -111,7 +111,7 @@ ADT Folder:" + $"\n {adtFolder}\n" +
             File.WriteAllLines(path, output);
         }
 
-        static void ParseAllADTs(ref List<string> wmos, ref List<string> m2s, string adtFolder)
+        static void ParseAllADTs(ref List<string> wmos, ref List<string> m2s, ref List<string> blps, string adtFolder)
         {
             foreach (var filePath in Directory.EnumerateFiles(adtFolder))
             {
@@ -120,6 +120,7 @@ ADT Folder:" + $"\n {adtFolder}\n" +
 
                 m2s.AddRange(ExtractM2PathsFromADT(filePath));
                 wmos.AddRange(ExtractWMOPathsFromADT(filePath));
+                blps.AddRange(ExtractBLPPathsFromADT(filePath));
 
                 Console.WriteLine($" {filePath}:\n  {m2s.Count} M2 paths\n  {wmos.Count} WMO paths");
             }
@@ -127,18 +128,33 @@ ADT Folder:" + $"\n {adtFolder}\n" +
 
         static void ParseAllWMOs(List<string> wmos, ref List<string> m2s, ref List<string> blps, string dataFolder)
         {
+            var subWmos = new List<string>();
             foreach (var wmoPath in wmos)
             {
                 string fullWmoPath = $"{dataFolder}\\{wmoPath}";
                 if (File.Exists(fullWmoPath))
                 {
                     ExtractAssetsFromWMO(ref m2s, ref blps, fullWmoPath);
+                    var pathPartA = wmoPath.Substring(wmoPath.LastIndexOf('\\') + 1);
+                    var file = pathPartA.Substring(0, pathPartA.LastIndexOf('.')).ToUpper();
+                    var folder = fullWmoPath.Substring(0, fullWmoPath.LastIndexOf('\\'));
+                    var parentShortFolder = folder.Substring(dataFolder.Length + 1);
+                    foreach (var filePath in Directory.EnumerateFiles(folder))
+                    {
+                        var subFilePath = filePath.Substring(filePath.LastIndexOf('\\') + 1);
+                        if (subFilePath.ToUpper().Contains(file))
+                        {
+                            subWmos.Add(parentShortFolder + "\\" + subFilePath);
+                            ExtractAssetsFromWMO(ref m2s, ref blps, filePath);
+                        }
+                    }
                 }
                 else
                 {
                     Console.WriteLine(" [ERROR] Unable to find file path: " + fullWmoPath);
                 }
             }
+            wmos.AddRange(subWmos);
         }
 
         static void ParseAllM2s(List<string> m2s, ref List<string> blps, string dataFolder)
@@ -149,6 +165,21 @@ ADT Folder:" + $"\n {adtFolder}\n" +
                 if (File.Exists(fullM2Path))
                 {
                     ExtractAssetsFromM2(ref blps, fullM2Path);
+                    var pathPartA = m2Path.Substring(m2Path.LastIndexOf('\\') + 1);
+                    var file = pathPartA.Substring(0, pathPartA.LastIndexOf('.')).ToUpper();
+                    var folder = fullM2Path.Substring(0, fullM2Path.LastIndexOf('\\'));
+                    var parentShortFolder = folder.Substring(dataFolder.Length + 1);
+                    foreach (var filePath in Directory.EnumerateFiles(folder))
+                    {
+                        // Don't have anything to store .skins, hack by inserting into blps which we don't do any processing on
+                        var subFilePath = filePath.Substring(filePath.LastIndexOf('\\') + 1).ToUpper();
+                        if (subFilePath.Contains(file) && 
+                            (subFilePath.EndsWith(".SKIN") || subFilePath.EndsWith(".BLP")))
+                        {
+                            blps.Add(parentShortFolder + "\\" + subFilePath);
+                        }
+                    }
+
                 }
                 else
                 {
@@ -233,11 +264,86 @@ ADT Folder:" + $"\n {adtFolder}\n" +
                 new int[] { 'N', 'D', 'O', 'M' },
                 new string[] { ".m2", ".mdx" }.ToList()));
 
-            blps.AddRange(ExtractStringListFromChunk(
-                path,
-                // XTOM = MOTX chunk
-                new int[] { 'X', 'T', 'O', 'M' },
-                new string[] { ".blp" }.ToList()));
+            // Read The MOMT chunk for offsets into MOTX chunk
+            var motxOffsets = GetWMOMOTXOffsetsFromMOMT(path);
+            blps.AddRange(GetWMOBLPFromMOTX(path, motxOffsets));
+        }
+
+        static List<string> GetWMOBLPFromMOTX(string path, List<uint> motxOffsets)
+        {
+            var blps = new List<string>();
+            // XTOM = MOTX chunk
+            long offset = FindChunkOffset(path, new int[] { 'X', 'T', 'O', 'M' }) + 4;
+            if (offset > 0)
+            {
+                using (var fileStream = new FileStream(path, FileMode.Open))
+                {
+                    using (var binReader = new BinaryReader(fileStream))
+                    {
+                        foreach (var motxOffset in motxOffsets)
+                        {
+                            fileStream.Position = offset + motxOffset;
+                            // Some offsets are near uint max, not sure why
+                            if (fileStream.Position < fileStream.Length)
+                            {
+                                var blp = ReadNullTerminatedString(binReader);
+                                // Sometimes reads rubbish, this algorithm isn't quite right
+                                if (blp.ToUpper().EndsWith(".BLP"))
+                                {
+                                    blps.Add(blp);
+                                }
+                                else
+                                {
+                                    Console.WriteLine(" [ERROR]: Invalid MOTX BLP: " + blp);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return blps;
+        }
+
+        static List<uint> GetWMOMOTXOffsetsFromMOMT(string path)
+        {
+            var motxOffsets = new List<uint>();
+            long offset = FindChunkOffset(path, new int[] { 'T', 'M', 'O', 'M' }) + 4;
+            if (offset > 0)
+            {
+                var numTextures = GetNumTexturesFromWMO(path);
+                using (var fileStream = new FileStream(path, FileMode.Open))
+                {
+                    fileStream.Position = offset;
+                    using (var binReader = new BinaryReader(fileStream))
+                    {
+                        for (var i = 0; i < numTextures; ++i)
+                        {
+                            fileStream.Position = fileStream.Position + 12;
+                            var tex1 = binReader.ReadUInt32();
+                            if (tex1 > 0)
+                                motxOffsets.Add(tex1);
+                            fileStream.Position = fileStream.Position + 8;
+                            var tex2 = binReader.ReadUInt32();
+                            if (tex2 > 0)
+                                motxOffsets.Add(tex2);
+                            fileStream.Position = fileStream.Position + 36;
+                        }
+                    }
+                }
+            }
+            return motxOffsets;
+        }
+
+        static uint GetNumTexturesFromWMO(string path)
+        {
+            using (var fileStream = new FileStream(path, FileMode.Open))
+            {
+                fileStream.Position = 20;
+                using (var binReader = new BinaryReader(fileStream))
+                {
+                    return binReader.ReadUInt32();
+                }
+            }
         }
 
         static List<string> ExtractM2PathsFromADT(string path)
@@ -258,6 +364,15 @@ ADT Folder:" + $"\n {adtFolder}\n" +
                 new string[] { ".wmo" }.ToList());
         }
 
+        static List<string> ExtractBLPPathsFromADT(string path)
+        {
+            return ExtractStringListFromChunk(
+                path,
+                // XETM = MTEX chunk
+                new int[] { 'X', 'E', 'T', 'M' },
+                new string[] { ".blp" }.ToList());
+        }
+
         static List<string> ExtractStringListFromChunk(string path, int[] chunk, List<string> extensionsValidated)
         {
             var list = new List<string>();
@@ -275,9 +390,13 @@ ADT Folder:" + $"\n {adtFolder}\n" +
                     {
                         var str = ReadNullTerminatedString(binReader);
                         if (str.Length == 0 || !ValidFileExtension(str, extensionsValidated))
+                        {
                             continueReading = false;
+                        }
                         else
+                        {
                             list.Add(str);
+                        }
                     }
                 }
             }
